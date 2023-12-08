f4bffa3ec390923feeceefd443810d2f27162775
Caique Araujo <caique@studiopiggly.com.br>
2023-12-08 15:21:08 -0300

    :pencil: text(styles): Home style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,8 @@ import React from 'react';
 
 export default function Home() {
 	return (
-		<main className="min-h-screen bg-slate-500">
-			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-				<div className="mx-auto max-w-3xl">
-					<h1>Lista de Tarefas</h1>
-				</div>
-			</div>
+		<main>
+			<h1>Lista de Tarefas</h1>
 		</main>
 	);
 }
